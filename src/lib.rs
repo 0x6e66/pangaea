019e83cce11b9b3ce39ed5ec5a3f7ef561686f83
Frondorf, Niklas <niklas.frondorf@web.de>
2024-01-29 14:10:18 +0100

    removed wrting to files in doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -13,16 +13,12 @@
 //! pub async fn main() {
 //!     let dataset_id = 820322;
 //!     let metadata = elastic_doc(dataset_id).await.unwrap();
-//!
-//!     let mut file = File::create(format!("pangaea-dataset-{}.json", dataset_id)).unwrap();
 //!     let json = serde_json::to_string(&metadata).unwrap();
-//!     write!(file, "{}", json).unwrap();
 //! }
 //! ```
 //!
 //! ## Search for multiple datasets
 //! ```
-//! use std::{fs::File, io::Write};
 //! use pangaea::elastic::elastic_search;
 //!
 //! #[tokio::main]
@@ -31,30 +27,13 @@
 //!         .await
 //!         .unwrap();
 //!
-//!     let mut file = File::create(format!("pangaea-datasets.jsonl")).unwrap();
-//!
 //!     res.into_iter()
 //!         .filter_map(|md_res| md_res.ok())
 //!         .for_each(|md| {
 //!             let json = serde_json::to_string(&md).unwrap();
-//!             writeln!(file, "{}", json).unwrap();
 //!         });
 //! }
 //! ```
-//!
-//!
-//! ## Download the actual data associated with the dataset
-//! ```
-//! use pangaea::download_data::download_data_by_id;
-//!
-//! #[tokio::main]
-//! async fn main() {
-//!     let id = 921673;
-//!     download_data_by_id(id, "downloaded_file").await.unwrap();
-//! }
-//!
-//! ```
-//! The data will saved to `filename.zip` or `filename.txt`, depending on the datatype that is returned
 
 pub mod download_data;
 pub mod elastic;
