b0e6f94bca132737265a3cf26afa6c751e0f6a13
Frondorf, Niklas <niklas.frondorf@web.de>
2024-02-16 14:41:31 +0100

    added support for bearer token authorization

--- examples/download_data.rs
+++ examples/download_data.rs
@@ -2,7 +2,16 @@ use pangaea::download_data::download_data_by_id;
 
 #[tokio::main]
 async fn main() {
+    download_zip().await;
+    download_txt().await;    
+}
+
+async fn download_zip(){
     let id = 921673;
-    // let id = 839111;
-    download_data_by_id(id, "downloaded_file").await.unwrap();
+    download_data_by_id(id, None, "downloaded_file").await.unwrap();
+}
+
+async fn download_txt(){
+    let id = 839111;
+    download_data_by_id(id, None, "downloaded_file").await.unwrap();
 }
--- src/download_data.rs
+++ src/download_data.rs
@@ -3,47 +3,66 @@
 use std::{fs::File, io::Write};
 
 use futures_util::StreamExt;
-use reqwest::{Client, Response};
+use reqwest::{
+    header::{HeaderMap, HeaderValue, AUTHORIZATION},
+    Client, Response,
+};
 
 use crate::prelude::*;
 
-/// Download the actual data that is accociated with the dataset with the id `dataset_id`.
+/// Download the actual data that is associated with the dataset with the id `dataset_id`.
 /// The data will saved to `filename.zip` or `filename.txt`, depending on the filetype.
-pub async fn download_data_by_id(dataset_id: u32, filename: impl Into<String>) -> Result<()> {
+pub async fn download_data_by_id(
+    dataset_id: u32,
+    bearer_token: Option<String>,
+    filename: impl Into<String>,
+) -> Result<()> {
     let base_url = format!("https://doi.pangaea.de/10.1594/PANGAEA.{}", dataset_id);
 
     let mut filename: String = filename.into();
 
+    let mut headers = HeaderMap::new();
+    if let Some(bearer_token) = bearer_token {
+        headers.insert(
+            AUTHORIZATION,
+            HeaderValue::from_str(&format!("Bearer {}", bearer_token)).map_err(|inv_header_val| {
+                Error::Generic(format!(
+                    "Could not convert the provided bearer token '{}' to a valid HeaderValue. Message: '{}'",
+                    bearer_token,
+                    inv_header_val
+                ))
+            })?,
+        );
+    }
+
     let client = Client::new();
+
     let response = client
         .get(&base_url)
         .query(&[("format", "zip")])
+        .headers(headers.clone())
         .send()
         .await?;
-
-    let correct_response = match response.status().is_success() {
-        true => {
-            filename.push_str(".zip");
-            response
-        }
-        false => {
-            filename.push_str(".txt");
-            client
-                .get(base_url)
-                .query(&[("format", "textfile")])
-                .send()
-                .await?
-        }
-    };
-
-    if !correct_response.status().is_success() {
-        return Err(Error::Generic(format!(
-            "Unable to locate data for dataset with id {}",
-            dataset_id
-        )));
+    if response.status().is_success() {
+        filename.push_str(".zip");
+        return write_response_to_file(response, filename).await;
     }
 
-    write_response_to_file(correct_response, filename).await
+    let response = client
+        .get(&base_url)
+        .query(&[("format", "textfile")])
+        .headers(headers)
+        .send()
+        .await?;
+    if response.status().is_success() {
+        filename.push_str(".txt");
+        return write_response_to_file(response, filename).await;
+    }
+
+    return Err(Error::Generic(format!(
+        "Unable to locate data for dataset with id '{}'",
+        dataset_id
+    )));
 }
 
 async fn write_response_to_file(response: Response, path: impl Into<String>) -> Result<()> {
