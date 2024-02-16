307140ddcb20edb5d975b01410cd1d7fa6e58aeb
Frondorf, Niklas <niklas.frondorf@web.de>
2024-02-16 12:08:18 +0100

    cargo fmt

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,5 @@
-use elasticsearch::{http::transport::Transport, Elasticsearch};
 use crate::prelude::*;
+use elasticsearch::{http::transport::Transport, Elasticsearch};
 
 pub fn get_elastic_client() -> Result<Elasticsearch> {
     let transport = Transport::single_node("https://ws.pangaea.de/es/pangaea")?;
